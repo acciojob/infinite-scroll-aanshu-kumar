f4d6654e4bd44c08d6980ec4f11b3dc781f6590f
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-24 14:33:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-//your code here!
-
+let arr=["Item 1","Item 2","Item 3","Item 4","Item 5","Item 6","Item 7","Item 8","Item 9"]
+let list=document.getElementById("infi-list");
+arr.map((item)=>{
+	list.innerHTML += `<li>${item}</li>`
+})
+let index=arr.length;
+list.addEventListener("scroll",()=>{
+        list.innerHTML +=`<li>Item ${++index}</li>`
+})
